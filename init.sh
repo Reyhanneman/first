46d0b34c7b2e011802b7fe8a357ec453090a2562
Reyhanneman <hvislyset@i.ua>
2018-07-13 12:30:12 +0000

    Some painful changes

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart 
+
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
