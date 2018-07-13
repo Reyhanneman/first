d4e80d0e281f9f4d85066de664f988e889c8b493
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-13 14:28:06 +0300

    Delete hello.py

--- etc/hello.py
+++
@@ -1,6 +0,0 @@
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-    body = b'Krasavcheg'
-    start_response(status, headers)
-    return iter([body])
