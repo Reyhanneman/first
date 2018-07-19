e115306fce82cbd2e672737cf295d1adf10fe43c
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-19 16:14:52 +0300

    Create urls.py

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [  
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/.*$', 'qa.views.test'),
+    url(r'^signup/.*$', 'qa.views.test'),
+    url(r'^question/\d+/$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/.*$', 'qa.views.test'),
+    url(r'^new/.*$', 'qa.views.test')
+]
