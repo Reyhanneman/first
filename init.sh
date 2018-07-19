fc20429efeefec599364305ab225496cb2f81fee
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-19 16:38:28 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
