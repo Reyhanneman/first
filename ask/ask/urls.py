3f5df0f5ee43de3ed78f9b1aa28f3aab123f76ac
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-20 11:52:24 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -34,14 +34,37 @@ Including another URLconf
 #   url(r'^new/.*$', 'qa.views.test')
 #]
 
-from django.contrib import admin
-from django.urls import path, include
+#from django.contrib import admin
+#from django.urls import path, include
 
-urlpatterns = [
-    url(r'^', include("qa.urls")),
-    url(r'^admin/', admin.site.urls),
-]
 #urlpatterns = [
-#    path('admin/', admin.site.urls),
-#    path('', include('qa.urls'))
-#]
+#    url(r'^', include("qa.urls")),
+#    url(r'^admin/', admin.site.urls),
+
+    
+    
+    
+    
+    
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from qa import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^question/', include('qa.urls')),
+    url(r'^$', include('qa.urls')),
+    url(r'^login/.*$',views.login_view),
+    url(r'^signup/.*$',views.signup),
+    url(r'^ask/.*$',views.question_add),
+    url(r'^popular/.*$',views.popular),
+    url(r'^answer/.*$',views.answer_add),
+    url(r'^question/(?P<question_id>[0-9]+)/answer/$', views.answer_add, name='answer_add'),
+    url(r'^new/.*$',views.test),
+    url(r'^admin/', include(admin.site.urls)),
+)
