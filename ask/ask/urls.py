fd76edb628be12aeda39e6cfb5cca6182b7e4be9
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-20 09:24:17 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -20,16 +20,25 @@ Including another URLconf
 #    url(r'^admin/', admin.site.urls),
 #]
 
-from django.conf.urls import include, url, patterns 
-from django.contrib import admin
-admin.autodiscover()
+#from django.conf.urls import include, url, patterns 
+#from django.contrib import admin
+#admin.autodiscover()
+#
+#urlpatterns = [  
+#    url(r'^$', 'qa.views.test'),
+#    url(r'^login/.*$', 'qa.views.test'),
+#    url(r'^signup/.*$', 'qa.views.test'),
+#    url(r'^question/\d+/$', 'qa.views.test'),
+#    url(r'^ask/.*$', 'qa.views.test'),
+#    url(r'^popular/.*$', 'qa.views.test'),
+#    url(r'^new/.*$', 'qa.views.test')
+#]
 
-urlpatterns = [  
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/.*$', 'qa.views.test'),
-    url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^question/\d+/$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/.*$', 'qa.views.test'),
-    url(r'^new/.*$', 'qa.views.test')
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('qa.urls'))
 ]
