2249c6c93817b747be39a7f950f192cc3ecfe846
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-20 16:26:27 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -39,15 +39,35 @@ Including another URLconf
 
 
 
-from django.conf.urls import include, url, patterns 
+#from django.conf.urls import include, url, patterns 
+#from django.contrib import admin
+#admin.autodiscover()
+
+#urlpatterns = [  
+#    url(r'^$', 'qa.views.test'),
+#    url(r'^login/.*$', 'qa.views.test'),
+#    url(r'^signup/.*$', 'qa.views.test'),
+#    url(r'^question/\d+/$', 'qa.views.test'),
+#    url(r'^ask/.*$', 'qa.views.test'),
+#    url(r'^popular/.*$', 'qa.views.test'),
+#    url(r'^new/.*$', 'qa.views.test')
+#]
+
+
+
+
+
+
+from django.conf.urls import include, url
+
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = [  
+urlpatterns = [
     url(r'^$', 'qa.views.test'),
     url(r'^login/.*$', 'qa.views.test'),
     url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^question/\d+/$', 'qa.views.test'),
+    url(r'^questions/\d+/$', 'qa.views.test'),
     url(r'^ask/.*$', 'qa.views.test'),
     url(r'^popular/.*$', 'qa.views.test'),
     url(r'^new/.*$', 'qa.views.test')
