f6ad03d9d4ad17827a57615d7a430f89cc047760
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-20 12:34:44 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ sudo /etc/init.d/nginx restart
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
 cd ask
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo python manage.py runserver
 
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
