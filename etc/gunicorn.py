4b99c1cf2e851f5d2aa6087382bd5c31ea43c18a
Reyhanneman <40892387+Reyhanneman@users.noreply.github.com>
2018-07-20 15:22:10 +0300

    Update and rename gunicorn.conf to gunicorn.py

---
+++ etc/gunicorn.py
@@ -0,0 +1,13 @@
+#CONFIG = {
+#    'mode': 'wsgi',
+#    'working_dir': '/home/box/web/',
+#    'args': (
+#        '--bind=0.0.0.0:8080',
+#       '--workers=4',
+#        '--timeout=60',
+#        'hello:app'
+#    )
+#}
+
+pythonpath = '/home/box/web'
+bind = "0.0.0.0:8080"
